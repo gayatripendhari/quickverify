d4062fd7d700ededb5fdec4aef361a1f16063829
gayatripendhari <gayatriapendhari123@gmail.com>
2024-03-14 19:56:15 +0530

    first commit

---
+++ src/test/java/com/firstspringbootproject/quickverify/QuickverifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstspringbootproject.quickverify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickverifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
